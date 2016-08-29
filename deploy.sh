9cf7d50967f6410578bf37c6b474d8ba3a11ecd9
Michael Viegas <michael.viegas@alldigitltd.co.uk>
2016-08-29 16:22:06 +0100

    fix(#39) WIP generate and deploy Nuget package script

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ev
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
-	nuget pack ./IgTradingFacade/IgTradingFacade.csproj -Version 1.0.$TRAVIS_BUILD_NUMBER -IncludeReferencedProjects -Prop Configuration=Release
-	nuget push ./*.nupkg $NUGET_API_KEY -Source https://www.nuget.org/api/v2/package
-fi
