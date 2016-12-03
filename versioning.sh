7a4fac59d4406fba7121cfaa1b4cd91566fd1fe9
Michael Viegas <michael.viegas@alldigitltd.co.uk>
2016-12-03 22:28:06 +0000

    chore(#45) versioning

--- versioning.sh
+++ versioning.sh
@@ -1,9 +1,9 @@
-set -f
+IFS='.' read -r -a PARSED <<< "$LATEST_TAG"
 
-PARSED=(${LATEST_TAG//:/ })
 latestMajor=${PARSED[0]}
 latestMinor=${PARSED[1]}
 latestRevision=${PARSED[2]}
+newRevision=0
 
 if [ $MAJOR == $latestMajor ] && [ $MINOR == $latestMinor ]; then
     newRevision=$(($latestRevision+1))
@@ -11,11 +11,10 @@ fi
 
 newVersion=$MAJOR"."$MINOR"."$newRevision
 
-echo $newVersion
-
 env -i git config --global user.email "builds@travis-ci.com"
 env -i git config --global user.name "Travis CI"
 env -i git tag $newVersion
 env -i git push origin --tags
 env -i nuget pack ./Alldigit.IG.TradingFacade.nuspec -Version $newVersion -IncludeReferencedProjects -Prop Configuration=Release
 env -i nuget push ./*.nupkg $NUGET_API_KEY -Source https://www.nuget.org/api/v2/package
+
