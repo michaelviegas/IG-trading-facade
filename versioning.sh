48448b4597be68c9e2faca7cfc67c20a193a5e7b
Michael Viegas <michael.viegas@alldigitltd.co.uk>
2016-12-03 21:05:21 +0000

    chore(#45) set git globals to push tags

--- versioning.sh
+++ versioning.sh
@@ -16,6 +16,8 @@ fi
 
 newVersion=$newMajor"."$newMinor"."$newRevision
 
+env -i git config --global user.email "builds@travis-ci.com"
+env -i git config --global user.name "Travis CI"
 env -i git tag $newVersion
 env -i git push origin --tags
 env -i nuget pack ./Alldigit.IG.TradingFacade.nuspec -Version $newVersion -IncludeReferencedProjects -Prop Configuration=Release
